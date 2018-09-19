be4d9bf2bd1aeb758e627135999539976a4184ae
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-19 01:38:47 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-  session.delete :name
-  redirect_to root_path
-end
+    session.delete :name
+    redirect_to root_path
+  end
   
 end
